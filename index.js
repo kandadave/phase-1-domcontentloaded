25eedbbd2879ba08b2aa83a2c360eb0cb49f8479
kandadave <kandachumo@gmail.com>
2025-06-19 08:21:29 +0300

    DOMcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const par = document.getElementById('text')
+    par.textContent = 'This is really cool!'
+});
+
